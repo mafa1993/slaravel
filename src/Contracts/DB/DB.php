e46348ffd8247d7f84b900d373fd862199ae42ec
Your Name <you@example.com>
2020-11-10 21:32:02 +0800

    接口绑定测试

---
+++ src/Contracts/DB/DB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/11/10 0010
+ * Time: 21:05
+ */
+
+namespace Slaravel\Contracts\DB;
+
+
+interface DB
+{
+    public function test();
+}
